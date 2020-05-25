dd55b97377f685cb13179b0541cc4db51490438e
blackwoods358 <65369957+blackwoods358@users.noreply.github.com>
2020-05-25 17:23:06 -0400

    Add files via upload
    Module 2 homework

---
+++ words.java
@@ -0,0 +1,86 @@
+package mod2;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class words {
+	//function to sort map
+	public static HashMap<String, Long> sortByValue(Map<String, Long> counts) 
+    { 
+ // Create a list From HashMap 
+    	 List<Entry<String, Long>> list = 
+                 new LinkedList<Map.Entry<String, Long> >(counts.entrySet());
+         
+// sort list with collections
+    	
+    	
+         Collections.sort(list, new Comparator<Map.Entry<String, Long> >() { 
+        	 
+             public int compare(Map.Entry<String, Long> o1,  
+                                Map.Entry<String, Long> o2)  
+             
+             { 
+
+                 return (o1.getValue()).compareTo(o2.getValue()); 
+             } 
+         }); 
+         
+         
+         
+ //Reverse the list
+         Collections.reverse(list);
+         
+         
+         // put data from sorted list to hashmap  
+         HashMap<String, Long> temp = new LinkedHashMap<String, Long>(); 
+         for (Map.Entry<String, Long> aa : list) { 
+             temp.put(aa.getKey(), aa.getValue()); 
+         } 
+        return temp; 
+    } 
+	
+    public static void main(String[] args) throws IOException {
+ //path for data
+        URL url = new URL("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm");
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line;
+        Map<String, Long> counts = new HashMap<>();
+
+ //Fill hash map  w values 
+while ((line = br.readLine()) != null) {
+        String[] words = line.split("[\\s.;,?:!()/<\"]+");
+            for (String word : words) {
+                word = word.trim();
+                String nline = br.readLine();
+                if (nline.equalsIgnoreCase("<P CLASS=\"poem\">"))
+                    continue;
+                if (nline.equalsIgnoreCase("</P>"))
+                    break;
+                if (word.length() > 0) {
+                    if (counts.containsKey(word)) {
+                        counts.put(word, counts.get(word) + 1);
+                    } else {
+                        counts.put(word, 1L);
+                    }
+                }
+
+            }
+        }
+     Map<String, Long> hm1 = sortByValue(counts);
+ // Display sorted Hashmap 
+     for (Map.Entry<String, Long> en : hm1.entrySet()) { 
+         System.out.println("Key = " + en.getKey() +  
+                       ", Value = " + en.getValue()); 
+  
+     		}
+    	}
+}
